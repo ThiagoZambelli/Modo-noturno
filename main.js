ee7710f59d6715fee2f6afa01caf56aff38b58c9
ThiagoZambelli <90356970+ThiagoZambelli@users.noreply.github.com>
2023-01-31 20:29:25 -0300

    Criação da aplicação de teste do modo Noturno

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector("#btn").addEventListener("click", () => {
+    document.body.classList.toggle("modo-dark");
+    document.querySelector("#mudar-tema").classList.toggle("modo-dark");
+})
